124f912b4b395d1f6dd779ec5562df9e920a6948
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2022-11-24 01:59:29 +0530

    Add Unit tests

--- src/module/__mocks__/comments.js
+++ src/module/__mocks__/comments.js
@@ -1,3 +1,5 @@
+import fetch from 'cross-fetch';
+
 const getComments = async (movieId) => {
   const response = await fetch(
     `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/V9PGHS19NclaPI0zbq7b/comments?item_id=${movieId}`
